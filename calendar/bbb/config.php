da0643a373a35f552fed77219279e93dda77309d
Alexandre Rocha Wendling <alexandrerw@celepar.pr.gov.br>
2016-06-23 13:06:19 -0300

    Issue #2 - Alterado o endereço url para webconf.pr.gov.br

--- calendar/bbb/config.php
+++ calendar/bbb/config.php
@@ -5,5 +5,4 @@ url: http://test-install.blindsidenetworks.com/bigbluebutton/
 salt: 8cd8ef52e8e101574e400365b55e11a6
 */		
 define("CONFIG_SECURITY_SALT", "5b48952aac6ab46f54cf04839627e56f");
-define("CONFIG_SERVER_BASE_URL", "http://webconf.pr.gov.br");
-?>
+define("CONFIG_SERVER_BASE_URL", "http://webconf.pr.gov.br/bigbluebutton/");
