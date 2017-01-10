6d2a30ef20e1f9191587435a7d2bcbc8f22a5817
Alexandre Correia <alexandrecorreia@celepar.pr.gov.br>
2017-01-10 14:15:23 -0200

    Issue #99 - Atualizada a conexão SOAP para aceitar certificado autoassinado.

--- phpgwapi/inc/class.SoapClientX.inc.php
+++ phpgwapi/inc/class.SoapClientX.inc.php
@@ -6,14 +6,28 @@ class SoapClientX extends SoapClient
 	
 	public function __construct( $wsdl, $options = array() )
 	{
-		parent::__construct( $wsdl, $options );
+		$context = stream_context_create( array(
+			'ssl' => array(
+						'verify_peer' => false,
+						'verify_peer_name' => false,
+						'allow_self_signed' => true
+					)
+				)
+			);
+	
+		$params = array_merge( [ 'stream_context' => $context ] , $options );
+
+		parent::__construct( $wsdl, $params );
+
 		$types =  array();
+
 		foreach( $this->__getTypes() as $type ) {
 			preg_match( '/^([^ ]*) ([^ ]*) {([^}]*)}$/', $type, $matches );
 			list( , , $name, $params ) = $matches;
 			preg_match_all( '/ ([^;]*) ([^;]*);/', $params, $matches );
 			$types[$name] = $matches[2];
 		}
+
 		foreach( $this->__getFunctions() as $prototype ) {
 			preg_match( '/^(.*) (.*)\((.*) /', $prototype, $matches );
 			$this->_function_types[$matches[2]] = $types[$matches[3]];
