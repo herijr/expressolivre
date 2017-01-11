6143814b794e5b29b17c5500904352ab852680c5
Alexandre Correia <alexandrecorreia@celepar.pr.gov.br>
2017-01-11 14:56:50 -0200

    Issue #102 - Corrigida a rotina para limpar a pasta lixeira/spam dentro da API.

--- api/rest/mail/CleanTrashResource.php
+++ api/rest/mail/CleanTrashResource.php
@@ -7,6 +7,7 @@ class CleanTrashResource extends MailAdapter {
 		$this->setResource("Mail","Mail/CleanTrash","Limpa a Lixeira do Usuário.",array("POST"));
 		$this->setIsMobile(true);
 		$this->addResourceParam("auth","string",true,"Chave de autenticação do Usuário.",false);
+		$this->addResourceParam("type","string",true,"(1 = Lixeira, 2 = Spam)",false,"1");
 
 	}
 
@@ -14,11 +15,28 @@ class CleanTrashResource extends MailAdapter {
 		// to Receive POST Params (use $this->params)
  		parent::post($request);
 
-		if($this-> isLoggedIn())
+		if( $this-> isLoggedIn() )
 		{
-			$params['clean_folder'] = 'imapDefaultTrashFolder';
-			if(!$this -> getImap() -> empty_folder($params))
+			$type = trim( $this->getParam('type') );
+
+			if( $type === "" )
+			{
+				$params = array( "type" => "trash" );
+			}
+			else
+			{	
+				switch( $type )
+				{
+					case "1" : $params = array( "type" => "trash" ); break;
+					case "2" : $params = array( "type" => "spam" ); break;
+					default  : $params = array( "type" => "trash" ); break;
+				}
+			}
+
+			if( !$this->getImap()->clean_folder( $params ) )
+			{
 				Errors::runException("MAIL_TRASH_NOT_CLEANED");
+			}
 		}
 
 		$this->setResult(true);
