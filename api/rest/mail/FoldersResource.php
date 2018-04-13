f663dd15ceee30d0475f5c7b01d36393b216fee4
Alexandre Correia <alexandrecorreia@ecelepar21133.celepar.parana>
2018-04-13 11:00:50 -0300

    Issue #244 - Corrigido para testar se existe o índice dentro do array quota_folders

--- api/rest/mail/FoldersResource.php
+++ api/rest/mail/FoldersResource.php
@@ -32,15 +32,15 @@ class FoldersResource extends MailAdapter {
 		foreach ($imap_folders as $i => $imap_folder) {
 			if(is_int($i)) {
 				$folder = array();
-				$folder['folderName'] 	  = mb_convert_encoding($imap_folder['folder_name'], "UTF8", "ISO_8859-1");
+				$folder['folderName'] 	  = mb_convert_encoding($imap_folder['folder_name'], "UTF-8", "ISO-8859-1");
 				 if(strtoupper($folder['folderName']) == 'INBOX') {				
 					$folder['folderName'] = $this->getImap()->functions->getLang("Inbox");
 				}
-				$folder['folderParentID'] = mb_convert_encoding($imap_folder['folder_parent'],'UTF8','ISO-8859-1');
+				$folder['folderParentID'] = mb_convert_encoding($imap_folder['folder_parent'],'UTF-8','ISO-8859-1');
 				$folder['folderHasChildren'] = $imap_folder['folder_hasChildren'];
 				$folder['qtdUnreadMessages'] = $imap_folder['folder_unseen'];
 				$folder['qtdMessages'] = $this->getImap()->get_num_msgs(array("folder" => $imap_folder['folder_id']));
-				$folder['folderID'] = mb_convert_encoding($imap_folder['folder_id'],'UTF8','ISO-8859-1');
+				$folder['folderID'] = mb_convert_encoding($imap_folder['folder_id'],'UTF-8','ISO-8859-1');
 		
 				if(substr($folder['folderID'], 0, 4) == 'user'){
 					$folder['folderType'] = "6";
@@ -59,27 +59,28 @@ class FoldersResource extends MailAdapter {
 		
 		$all_folders = array_merge($default_folders, $user_folders, $shared_folders);
 		$quota_folders = $this->getImap()->get_quota_folders();
-		$search = $this->getParam('search') ? mb_convert_encoding($this->getParam('search'),"ISO_8859-1", "UTF8") : null;
+		$search = $this->getParam('search') ? mb_convert_encoding($this->getParam('search'),"ISO-8859-1", "UTF-8") : null;
+		
 		foreach($all_folders as $i => $folder){
-			$folder_name = mb_convert_encoding($folder['folderName'],"ISO_8859-1", "UTF8");
-			$folder_id   = mb_convert_encoding($folder['folderID'],"ISO_8859-1", "UTF8");
+			$folder_name = mb_convert_encoding($folder['folderName'],"ISO-8859-1", "UTF-8");
+			$folder_id   = mb_convert_encoding($folder['folderID'],"ISO-8859-1", "UTF-8");
 			if($folder_id == 'INBOX') {
 				$j = $this->getImap()->functions->getLang("Inbox");
 			}else {
 				$j = str_replace("INBOX".$this->getImap()->imap_delimiter,"",$folder_id);
 			}
-			$all_folders[$i]['diskSizeUsed'] 	= $quota_folders[$j]['quota_used'];
-			$all_folders[$i]['diskSizePercent'] = $quota_folders[$j]['quota_percent']/100;
+			$all_folders[$i]['diskSizeUsed'] 	= ( isset($quota_folders[$j]['quota_used']) ? $quota_folders[$j]['quota_used'] : "0" );
+			$all_folders[$i]['diskSizePercent'] = ( isset($quota_folders[$j]['quota_percent']) ? $quota_folders[$j]['quota_percent']/100 : "0" );
 			if($search != null && stristr($folder_name, $search) == null){
 				unset($all_folders[$i]);
 			}
 		}
-		
+
 		$result = array (
 				'folders' => array_values($all_folders),
-				'diskSizeUsed'     => $quota_folders['quota_root']['quota_used']*1024,
-				'diskSizeLimit'    => $quota_folders['quota_root']['quota_limit']*1024,
-				'diskSizePercent'  => $quota_folders['quota_root']['quota_percent']/100
+				'diskSizeUsed'     => ( isset($quota_folders['quota_root']['quota_used'])? $quota_folders['quota_root']['quota_used']*1024: "0" ),
+				'diskSizeLimit'    => ( isset($quota_folders['quota_root']['quota_limit'])? $quota_folders['quota_root']['quota_limit']*1024: "0" ),
+				'diskSizePercent'  => ( isset($quota_folders['quota_root']['quota_percent'])? $quota_folders['quota_root']['quota_percent']/100: "0" )
 		);			
 		
 		$this->setResult($result);
