29a5fc500e73f5c2209d3aec06df8286b4503993
Alexandre Luiz Correia <alexandrecorreia@celepar.pr.gov.br>
2018-08-15 17:24:01 -0300

    Issue #328 - Adicionado instrução para ler do arquivo de configuração a lista de apis disponíveis.

--- api/rest/core/UserApiResource.php
+++ api/rest/core/UserApiResource.php
@@ -15,87 +15,74 @@ class UserApiResource extends ExpressoAdapter {
 		// to Receive POST Params (use $this->params)
  		parent::post($request);
 
- 		$apis = array( 
- 			"https://api.expresso.pr.gov.br/", 
- 			"https://api.expresso.pr.gov.br/municipios/", 
- 			"https://api.expresso.pr.gov.br/seed/", 
- 			"https://api.expresso.pr.gov.br/sefa/", 
- 			"https://api.expresso.pr.gov.br/sesp/"
- 		);
-
- 		$resource = "UserApps";
-
- 		$user_id = $this->getParam('user');
- 		$modules = $this->getParam('modules');
-
- 		$modulesArray	= array();
-
-		if( strrpos( $modules, ",") !== FALSE )
-		{
-			$modulesArray = explode(",", $modules );
-		}
-		else
-		{
-			if ($modules != "") {
-				$modulesArray[0] = $modules;
-			}
-			
-		}
-
-
-		$response = array();
-		$params = array();
-		$params['user'] = $user_id;		
-
-		$i = 0;
-
-		$predictedAPI = "";
-
-		foreach ($apis as $api) {
-
-			$result = $this->callBase($api . $resource, $params);
-
-			$arr_res = json_decode($result);
-			
-			$apps = $arr_res->result->apps; 
-
-			if (!is_array($apps)) {
-				$apps = array();
-			}
-			$qtdFound = 0;
-			foreach( $modulesArray as $moduleName )
-			{
-				foreach ($apps as $appName) {
-					if ($moduleName == $appName) {
-						$qtdFound = $qtdFound + 1;
-					}
-				}	
-			}
-
-			if (count($modulesArray) != 0) {
-				if ($qtdFound == count($modulesArray)) {
-					$predictedAPI = $api;
-				}
-			} else {
-				if (count($apps) != 0) {
-					$predictedAPI = $api;
-				}
-			}
-
-			$response['apis'][$i]["api"] = $api;
-			$response['apis'][$i]['apps'] = $apps;
-			$i++;
-		}
-
-		if ($predictedAPI != "") {
-			$response['userAPI'] = $predictedAPI;
-			$this->setResult($response);
+		if( !file_exists( __DIR__ . '/../../config/profileHomeServer.ini') ){
+			Errors::runException(2201);
 		} else {
-			Errors::runException(2200);
-		}
 
- 		
-		return $this->getResponse();
+			$profiles	= parse_ini_file( __DIR__ . '/../../config/profileHomeServer.ini', true);
+			$profileValues = array();
+			
+			foreach( $profiles['home.server'] as $key => $values ){
+				$profileValues[] = trim($values);
+			}
+
+			$apis = array_unique($profileValues);
+	 		$resource = "UserApps";
+	 		$user_id = $this->getParam('user');
+	 		$modules = $this->getParam('modules');
+	 		$modulesArray	= array();
+
+			if( strrpos( $modules, ",") !== FALSE ) {
+				$modulesArray = explode(",", $modules );
+			} else {
+				if ($modules != "") {
+					$modulesArray[0] = $modules;
+				}
+			}
+
+			$response = array();
+			$params = array();
+			$params['user'] = $user_id;		
+
+			$i = 0;
+
+			$predictedAPI = "";
+
+			foreach( $apis as $api ){
+				$result = $this->callBase($api . $resource, $params);
+				$arr_res = json_decode($result);
+				$apps = $arr_res->result->apps; 
+				
+				if (!is_array($apps)) {
+					$apps = array();
+				}
+				$qtdFound = 0;
+				foreach( $modulesArray as $moduleName )
+				{
+					foreach ($apps as $appName) {
+						if ($moduleName == $appName) { $qtdFound = $qtdFound + 1; }
+					}	
+				}
+				
+				if (count($modulesArray) != 0) {
+					if ($qtdFound == count($modulesArray)) { $predictedAPI = $api; }
+				} else {
+					if (count($apps) != 0) { $predictedAPI = $api; }
+				}
+				
+				$response['apis'][$i]["api"] = $api;
+				$response['apis'][$i]['apps'] = $apps;
+				$i++;
+			}
+
+			if ($predictedAPI != "") {
+				$response['userAPI'] = $predictedAPI;
+				$this->setResult($response);
+			} else {
+				Errors::runException(2200);
+			}
+			return $this->getResponse();
+		}
 	}
 
 	public function callBase($url,$params) {
