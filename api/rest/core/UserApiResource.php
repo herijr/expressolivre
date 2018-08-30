cdf61a349580f961ea9f96276ecb5eb80cf89865
Mauricio Luiz Viani <viani@celepar.pr.gov.br>
2018-08-30 14:34:44 -0300

    Issue #352 - Adição de barra na url de apis

--- api/rest/core/UserApiResource.php
+++ api/rest/core/UserApiResource.php
@@ -10,7 +10,7 @@ class UserApiResource extends ExpressoAdapter {
 		$this->addResourceParam("modules","string",false,"Modulos esperados que o usuario tenha (separados por virgula).");
 
 	}
-	
+
 	public function post($request){
 		// to Receive POST Params (use $this->params)
  		parent::post($request);
@@ -21,12 +21,14 @@ class UserApiResource extends ExpressoAdapter {
 
 			$profiles	= parse_ini_file( __DIR__ . '/../../config/profileHomeServer.ini', true);
 			$profileValues = array();
-			
-			foreach( $profiles['home.server'] as $key => $values ){
-				$profileValues[] = trim($values);
+
+			foreach( $profiles['home.server'] as $key => $value ) {
+				$value =  preg_match('/\/$/', trim($value)) ? trim($value) : trim($value) . "/";
+				$profileValues[] = $value;
 			}
 
 			$apis = array_unique($profileValues);
+
 	 		$resource = "UserApps";
 	 		$user_id = $this->getParam('user');
 	 		$modules = $this->getParam('modules');
@@ -42,7 +44,7 @@ class UserApiResource extends ExpressoAdapter {
 
 			$response = array();
 			$params = array();
-			$params['user'] = $user_id;		
+			$params['user'] = $user_id;
 
 			$i = 0;
 
@@ -51,8 +53,8 @@ class UserApiResource extends ExpressoAdapter {
 			foreach( $apis as $api ){
 				$result = $this->callBase($api . $resource, $params);
 				$arr_res = json_decode($result);
-				$apps = $arr_res->result->apps; 
-				
+				$apps = $arr_res->result->apps;
+
 				if (!is_array($apps)) {
 					$apps = array();
 				}
@@ -61,15 +63,15 @@ class UserApiResource extends ExpressoAdapter {
 				{
 					foreach ($apps as $appName) {
 						if ($moduleName == $appName) { $qtdFound = $qtdFound + 1; }
-					}	
+					}
 				}
-				
+
 				if (count($modulesArray) != 0) {
 					if ($qtdFound == count($modulesArray)) { $predictedAPI = $api; }
 				} else {
 					if (count($apps) != 0) { $predictedAPI = $api; }
 				}
-				
+
 				$response['apis'][$i]["api"] = $api;
 				$response['apis'][$i]['apps'] = $apps;
 				$i++;
@@ -94,7 +96,7 @@ class UserApiResource extends ExpressoAdapter {
 
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		
+
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $newPost);
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
