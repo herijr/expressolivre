c5beb40b3ac4e6de5099b1e6b7f4b98cfa8d066a
Alexandre Luiz Correia <alexandrecorreia@celepar.pr.gov.br>
2019-04-09 15:31:29 -0300

    Issue #433 - Refatorado o código para listar as pastas dentro da API.

--- api/rest/mail/FoldersResource.php
+++ api/rest/mail/FoldersResource.php
@@ -16,74 +16,48 @@ class FoldersResource extends MailAdapter {
 	public function post($request){
 		// to Receive POST Params (use $this->params)		
  		parent::post($request);	 		
- 		
-		if($this-> isLoggedIn()) 
-		{	
-			$imap_folders =  $this->getImap()->get_folders_list();
-		}
-		if(!$imap_folders){
-			return $this->getResponse();
-		}
-			
-		$all_folders    = array();
-		$user_folders   = array();
-		$shared_folders = array();
-		
-		foreach ($imap_folders as $i => $imap_folder) {
-			if(is_int($i)) {
-				$folder = array();
-				$folder['folderName'] 	  = mb_convert_encoding($imap_folder['folder_name'], "UTF-8", "ISO-8859-1");
-				 if(strtoupper($folder['folderName']) == 'INBOX') {				
-					$folder['folderName'] = $this->getImap()->functions->getLang("Inbox");
-				}
-				$folder['folderParentID'] = mb_convert_encoding($imap_folder['folder_parent'],'UTF-8','ISO-8859-1');
-				$folder['folderHasChildren'] = $imap_folder['folder_hasChildren'];
-				$folder['qtdUnreadMessages'] = $imap_folder['folder_unseen'];
-				$folder['qtdMessages'] = $this->getImap()->get_num_msgs(array("folder" => $imap_folder['folder_id']));
-				$folder['folderID'] = mb_convert_encoding($imap_folder['folder_id'],'UTF-8','ISO-8859-1');
-		
-				if(substr($folder['folderID'], 0, 4) == 'user'){
-					$folder['folderType'] = "6";
-					$shared_folders[] = $folder;
-				}else if(array_key_exists($folder['folderID'], $this->defaultFolders) !== false) {
-					$folder_type = $this->defaultFolders[$folder['folderID']];
-					$folder['folderType'] = $folder_type;
-					$default_folders[] = $folder;
-				}
-				else{
-					$folder['folderType'] = "5";
-					$user_folders[] = $folder;
-				}
-			}
-		}
-		
-		$all_folders = array_merge($default_folders, $user_folders, $shared_folders);
-		$quota_folders = $this->getImap()->get_quota_folders();
-		$search = $this->getParam('search') ? mb_convert_encoding($this->getParam('search'),"ISO-8859-1", "UTF-8") : null;
-		
-		foreach($all_folders as $i => $folder){
-			$folder_name = mb_convert_encoding($folder['folderName'],"ISO-8859-1", "UTF-8");
-			$folder_id   = mb_convert_encoding($folder['folderID'],"ISO-8859-1", "UTF-8");
-			if($folder_id == 'INBOX') {
-				$j = $this->getImap()->functions->getLang("Inbox");
-			}else {
-				$j = str_replace("INBOX".$this->getImap()->imap_delimiter,"",$folder_id);
-			}
-			$all_folders[$i]['diskSizeUsed'] 	= ( isset($quota_folders[$j]['quota_used']) ? $quota_folders[$j]['quota_used'] : "0" );
-			$all_folders[$i]['diskSizePercent'] = ( isset($quota_folders[$j]['quota_percent']) ? $quota_folders[$j]['quota_percent']/100 : "0" );
-			if($search != null && stristr($folder_name, $search) == null){
-				unset($all_folders[$i]);
-			}
-		}
 
-		$result = array (
-				'folders' => array_values($all_folders),
-				'diskSizeUsed'     => ( isset($quota_folders['quota_root']['quota_used'])? $quota_folders['quota_root']['quota_used']*1024: "0" ),
-				'diskSizeLimit'    => ( isset($quota_folders['quota_root']['quota_limit'])? $quota_folders['quota_root']['quota_limit']*1024: "0" ),
-				'diskSizePercent'  => ( isset($quota_folders['quota_root']['quota_percent'])? $quota_folders['quota_root']['quota_percent']/100: "0" )
-		);			
-		
-		$this->setResult($result);
+		if ($this->isLoggedIn()) {
+			$folders = $this->getImap()->get_folders_list(array("onload" => true));
+
+			$result = array();
+
+			foreach ($folders as $key => $value) {
+				if (is_int($key)) {
+
+					$folderName = mb_convert_encoding($value['folder_name'], "UTF-8", "ISO-8859-1");
+					$folderName = (strtoupper($folderName) === "INBOX" ? $this->getImap()->functions->getLang("Inbox") : $folderName);
+					$folderParentID = mb_convert_encoding($value['folder_parent'], 'UTF-8', 'ISO-8859-1');
+					$folderHasChildren = $value['folder_hasChildren'];
+
+					$folderID = mb_convert_encoding($value['folder_id'], 'UTF-8', 'ISO-8859-1');
+					$folderID = trim($folderID);
+
+					$folderType = (substr($value['folder_id'], 0, 4) == "user" ? "6" : "5");
+					$folderType = array_key_exists($folderID, $this->defaultFolders) !== false ? strval($this->defaultFolders[$folderID]) : $folderType;
+
+					$qtdUnreadMessages = $value['folder_unseen'];
+
+					$result['folders'][] = array(
+						'folderName' => $folderName,
+						'folderParentID' => $folderParentID,
+						'folderHasChildren' => $folderHasChildren,
+						'qtdUnreadMessages' => $qtdUnreadMessages,
+						'qtdMessages' => '0',
+						'folderID' => $folderID,
+						'folderType' => $folderType,
+						'diskSizeUsed' => '0',
+						'diskSizePercent' => '0',
+					);
+				}
+			}
+
+			$result["diskSizeUsed"] = intval($folders['quota_used']) > 0 ? $folders['quota_used'] * 1024 : 0;
+			$result["diskSizeLimit"] = intval($folders['quota_limit']) > 0 ? $folders['quota_limit'] * 1024 : 0;
+			$result["diskSizePercent"] = intval($folders['quota_percent']) > 0 ? $folders['quota_percent'] / 100 : 0;
+
+			$this->setResult($result);
+		}
 		
 		//to Send Response (JSON RPC format)
 		return $this->getResponse();		
